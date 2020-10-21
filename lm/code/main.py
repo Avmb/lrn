3f48ea6775b20213e3c57612758c235196ba6b08
Antonio Valerio Miceli Barone <amiceli@inf.ed.ac.uk>
2020-10-21 18:30:15 +0100

    fix parallelism 2

--- lm/code/main.py
+++ lm/code/main.py
@@ -169,7 +169,7 @@ def evaluate(data_source, batch_size=10):
             data, targets = get_batch(data_source, i, args)
             targets = targets.view(-1)
 
-            log_prob, hidden = parallel_model(data, hidden, average_ensemble=True)
+            log_prob, hidden = parallel_model(hidden, input=data, average_ensemble=True)
             loss = nn.functional.nll_loss(log_prob.view(-1, log_prob.size(2)), targets).data
 
             total_loss += loss * len(data)
@@ -209,7 +209,7 @@ def train():
             # If we didn't, the model would try backpropagating all the way to start of the dataset.
             hidden[s_id] = repackage_hidden(hidden[s_id])
 
-            log_prob, hidden[s_id], rnn_hs, dropped_rnn_hs, student_distill_loss = parallel_model(cur_data, hidden[s_id], return_h=True, return_student_distill_loss=True)
+            log_prob, hidden[s_id], rnn_hs, dropped_rnn_hs, student_distill_loss = parallel_model(hidden[s_id], input=cur_data, return_h=True, return_student_distill_loss=True)
             raw_loss = nn.functional.nll_loss(log_prob.view(-1, log_prob.size(2)), cur_targets)
 
             loss = raw_loss
